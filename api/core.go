19bbdfaf9f87b44f37fe5cbba4cf0d13b74e0f08
egodasa <egodasa@gmail.com>
2020-04-12 13:13:25 +0700

    Menambahkan komentar

--- api/core.go
+++ api/core.go
@@ -1,8 +1,8 @@
 package api
 
 type CoreApi struct {
-  HttpStatus int `json:"code"`
-  Messages string `json:"message,omitempty"`
-  Data interface{} `json:"data,omitempty"`
+  HttpStatus int `json:"code"` // field untuk kode http
+  Messages string `json:"message,omitempty"` // field untuk pesan
+  Data interface{} `json:"data,omitempty"` // field yang menamping data yang akan diberikan ke klien
 }
 
