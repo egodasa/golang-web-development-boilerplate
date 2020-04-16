7003e1b0b38e1fd5039a57979e2c375ca8886e56
egodasa <egodasa@gmail.com>
2020-04-16 14:04:27 +0700

    Update database

--- db_mobil.sql
+++ db_mobil.sql
@@ -5,23 +5,53 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `tb_jenis_mobil`;
+CREATE TABLE `tb_jenis_mobil` (
+  `id_jenis` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_jenis` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_jenis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tb_jenis_mobil` (`id_jenis`, `nm_jenis`) VALUES
+(1,	'Low Cost Green Car'),
+(2,	'Low Sport Utility Vehicle'),
+(3,	'Sport Utility Vehicle'),
+(4,	'Multi Purpose Vehicle'),
+(5,	'Sedan'),
+(6,	'Truck'),
+(7,	'Sport'),
+(8,	'City Car');
+
 DROP TABLE IF EXISTS `tb_mobil`;
 CREATE TABLE `tb_mobil` (
   `id_mobil` int(11) NOT NULL AUTO_INCREMENT,
-  `kode_mobil` varchar(10) NOT NULL,
-  `merk` varchar(50) NOT NULL,
-  `tipe` varchar(100) NOT NULL,
-  `harga` int(11) NOT NULL,
-  `warna` varchar(20) NOT NULL,
-  `penggerak` varchar(4) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `nm_mobil` varchar(100) NOT NULL,
+  `jenis_penggerak` varchar(5) NOT NULL,
   `banyak_roda` int(11) NOT NULL,
-  `banyak_bangku` int(11) NOT NULL,
-  `jenis_mesin` varchar(20) NOT NULL,
-  `mesin` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_mobil`)
+  `id_jenis` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  PRIMARY KEY (`id_mobil`),
+  KEY `id_perusahaan` (`id_perusahaan`),
+  KEY `id_jenis` (`id_jenis`),
+  CONSTRAINT `tb_mobil_ibfk_1` FOREIGN KEY (`id_perusahaan`) REFERENCES `tb_perusahaan` (`id_perusahaan`) ON DELETE CASCADE,
+  CONSTRAINT `tb_mobil_ibfk_2` FOREIGN KEY (`id_jenis`) REFERENCES `tb_jenis_mobil` (`id_jenis`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tb_mobil` (`id_mobil`, `kode_mobil`, `merk`, `tipe`, `harga`, `warna`, `penggerak`, `banyak_roda`, `banyak_bangku`, `jenis_mesin`, `mesin`) VALUES
-(1,	'T001',	'Toyota',	'Kijang Innova Venturer',	675000000,	'Merah',	'FR',	4,	6,	'Diesel',	'D4D');
+INSERT INTO `tb_mobil` (`id_mobil`, `id_perusahaan`, `nm_mobil`, `jenis_penggerak`, `banyak_roda`, `id_jenis`, `harga`) VALUES
+(3,	1,	'Kijang Innova Venturer',	'FR',	4,	3,	675000000);
 
--- 2020-04-13 09:53:35
+DROP TABLE IF EXISTS `tb_perusahaan`;
+CREATE TABLE `tb_perusahaan` (
+  `id_perusahaan` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_perusahaan` varchar(50) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_perusahaan`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tb_perusahaan` (`id_perusahaan`, `nm_perusahaan`, `alamat`) VALUES
+(1,	'Toyota',	''),
+(2,	'Suzuki',	''),
+(3,	'Honda',	'');
+
+-- 2020-04-16 07:04:04
