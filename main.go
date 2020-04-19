616af4b836c6a15a401e9999d68d2b18aed0a44a
egodasa <egodasa@gmail.com>
2020-04-19 21:16:42 +0700

    Perbaikan main

--- main.go
+++ main.go
@@ -4,23 +4,22 @@ import (
 	"belajar-ech0-framework/config"
 	"belajar-ech0-framework/routes"
 	"os"
-
+  
+  "github.com/gin-gonic/gin"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql" // import your required driver
 )
 
 func init() {
 	config.LoadConfig()
-	dbConnection := os.Getenv("DB_CONNECTION")
-	dbString := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")" + "/" + os.Getenv("DB_DATABASE")
+	var dbConnection string = os.Getenv("DB_CONNECTION")
+	var dbString string = os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")" + "/" + os.Getenv("DB_DATABASE")
 	orm.RegisterDataBase("default", dbConnection, dbString, 30)
 }
 
 func main() {
-	config.LoadConfig()       // ambil konfigurasi aplikasi
-	server := routes.Router() // ambil routes
-	// ambil nilai port dari file .env
-	port := ":" + os.Getenv("APP_PORT")
-
+  config.LoadConfig()// ambil konfigurasi aplikasi
+  var server *gin.Engine = routes.Router() // ambil routes
+  var port string = ":" + os.Getenv("APP_PORT")
 	server.Start(port)
 }
