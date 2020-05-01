22383af356fd68499391b5e439e48a43f07d0c8f
egodasa <egodasa@gmail.com>
2020-05-01 12:36:48 -0700

    Penambahan contoh custom function di model dan api

--- api/mobil.go
+++ api/mobil.go
@@ -1,20 +1,30 @@
 package api
 
 import (
-    md "belajar-ech0-framework/models"
-    "net/http"
+	md "belajar-ech0-framework/models"
+	"net/http"
+
+	gin "github.com/gin-gonic/gin"
 )
 
 // vairabel ini akan dipakai di route
 type ApiMobil struct {
-  *CoreApi
+	*CoreApi
+}
+
+func (c ApiMobil) Get(ctx *gin.Context) {
+	var data, err = md.ModelMobil.GetMobil()
+	if err == true {
+		c.HttpStatus = http.StatusInternalServerError
+		c.Messages = "Terjadi kesalahan dalam mengambil data"
+	} else {
+		c.Data = data
+	}
+	ctx.JSON(c.HttpStatus, c)
 }
 
 var Mobil ApiMobil = ApiMobil{&CoreApi{
-  HttpStatus: http.StatusOK,
-  Messages: "Ok",
-  ApiModels: md.ModelMobil,
+	HttpStatus: http.StatusOK,
+	Messages:   "Ok",
+	ApiModels:  md.ModelMobil,
 }}
-
-
-
--- models/mobil.go
+++ models/mobil.go
@@ -1,45 +1,51 @@
 package models
 
+import (
+  hpl "belajar-ech0-framework/helper"
+  "fmt"
+  orm "github.com/astaxie/beego/orm"
+  _ "github.com/go-sql-driver/mysql" // import your required driver
+)
+
 var MobilColumn = map[string]Column{
   "id_mobil": Column{
-    Name: "id_mobil",
-    Type: "int",
-    Fillable: false,
-    IsPk: true,
+    Name:          "id_mobil",
+    Type:          "int",
+    Fillable:      false,
+    IsPk:          true,
     AutoIncrement: true,
   },
   "id_perusahaan": Column{
-    Name: "id_perusahaan",
-    Type: "int",
+    Name:     "id_perusahaan",
+    Type:     "int",
     Fillable: true,
   },
   "nm_mobil": Column{
-    Name: "nm_mobil",
-    Type: "varchar",
+    Name:     "nm_mobil",
+    Type:     "varchar",
     Fillable: true,
   },
   "jenis_penggerak": Column{
-    Name: "jenis_penggerak",
-    Type: "varchar",
+    Name:     "jenis_penggerak",
+    Type:     "varchar",
     Fillable: true,
   },
   "banyak_roda": Column{
-    Name: "banyak_roda",
-    Type: "int",
+    Name:     "banyak_roda",
+    Type:     "int",
     Fillable: true,
   },
   "id_jenis": Column{
-    Name: "id_jenis",
-    Type: "int",
+    Name:     "id_jenis",
+    Type:     "int",
     Fillable: true,
   },
   "harga": Column{
-    Name: "harga",
-    Type: "int",
+    Name:     "harga",
+    Type:     "int",
     Fillable: true,
   },
-} 
-
+}
 
 // inisaliasi model mobil
 // nanti variabel ini akan digunakan di controller
@@ -47,5 +53,28 @@ type Mobil struct {
   *Models
 }
 
-var ModelMobil Mobil = Mobil{NewModels("tb_mobil", MobilColumn)}
+func (m *Mobil) GetMobil() (result []orm.Params, isError bool) {
+  Db := m.GetDb()
+  sqlColumn := []string{
+    "tb_mobil.*",
+    "tb_jenis_mobil.nm_jenis",
+    "tb_perusahaan.nm_perusahaan",
+    "tb_perusahaan.alamat",
+  }
+  sqlWhere := map[string]interface{}{
+    "[><] tb_jenis_mobil": "tb_mobil.id_jenis = tb_jenis_mobil.id_jenis",
+    "[><] tb_perusahaan":  "tb_mobil.id_perusahaan = tb_perusahaan.id_perusahaan",
+  }
 
+  sql, args, _ := hpl.SelectSql(m.GetTableName(), sqlColumn, sqlWhere)
+  _, err := Db.Raw(sql, args).Values(&result)
+
+  if err != nil {
+    fmt.Println(err.Error())
+    isError = true
+  }
+
+  return result, isError
+}
+
+var ModelMobil Mobil = Mobil{NewModels("tb_mobil", MobilColumn)}
